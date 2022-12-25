94aede224330002172c827118d2dc54114bd64db
Douglas Soares <douglasmaismcpe@gmail.com>
2022-12-25 15:08:28 -0200

    Init Project

---
+++ src/middlewares/errorMiddleware.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../errors/AppError";
+
+export function errorMiddleware(
+  error: Error, 
+  request: Request, 
+  response: Response, 
+  next: NextFunction
+) {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({ message: error.message});
+  }
+
+  return response.status(500).json({
+    message: `Internal server error! - ${error.message}`
+  });
+}
